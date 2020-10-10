55834ccec44e674da0e476515f9f3697184f044f
Fendi Setiawan <fendi.septiawan0709@gmail.com>
2020-10-10 14:10:14 +0700

    Advance set operation course

---
+++ 06_advance_set_operations/main.py
@@ -0,0 +1,11 @@
+maths = {'Algebra', 'Volume', 'Pythagoras', 'Scaling'}
+art = {'Sketch', 'Scaling', 'Colour'}
+
+# Union (Joining all item and remove duplicates)
+print(maths.union(art))
+
+# Intersect (Pointing out the same value)
+print(maths.intersection(art))
+
+# Difference (Find difference from first set)
+print(maths.difference(art))
