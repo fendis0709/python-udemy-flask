33d51534a5358de8802765ee86fc46ef66c3a83a
Fendi Setiawan <fendi.septiawan0709@gmail.com>
2020-10-10 11:49:11 +0700

    01 - Variable

---
+++ 01_variables/main.py
@@ -0,0 +1,15 @@
+# Simple variable assignment
+x = 10
+print(x)
+
+# Simple variable calculation
+price = 8.99
+discount = 0.2
+result = price * (1 - discount)
+print(result)
+
+# String variable
+name = 'Joko'
+print(name)
+print(name + name)          # Concatenation
+print(name * 3)             # String repeat
