bb775512bdebf41b1101e7b01175f39feee15cc6
Fendi Setiawan <fendi.septiawan0709@gmail.com>
2020-10-10 12:24:59 +0700

    User input course

---
+++ 03_user_input/main.py
@@ -0,0 +1,5 @@
+size = input('How large is your smartphone screen (in inches): ')
+
+# Let's begin some calculation
+sizeInCentimeter = int(size) * 2.54
+print(f'Your screen in centimeters is : {sizeInCentimeter:.2f} centimeters')
