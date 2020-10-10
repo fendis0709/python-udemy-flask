b3edc54b08621df4abb1c6812cb7aef4bdb78009
Fendi Setiawan <fendi.septiawan0709@gmail.com>
2020-10-10 14:33:34 +0700

    If statement course

---
+++ 08_if_statements/main.py
@@ -0,0 +1,11 @@
+today = input('Sekarang hari apa ya...? ')
+today = today.lower()
+
+if today == 'senin' :
+    print('Semangat bung...!')
+elif today == 'minggu' :
+    print('Selamat berlibur.')
+else :
+    print('Lanjutkan.')
+
+print('Stay safe bro')
