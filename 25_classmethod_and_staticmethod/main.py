00241158ff1c8bda9bc1e831f945a4b63096f138
Fendi Septiawan <fendi.septiawan0709@gmail.com>
2020-10-13 11:37:03 +0700

    Class method and Static method course

---
+++ 25_classmethod_and_staticmethod/main.py
@@ -0,0 +1,23 @@
+class Person :
+    MARRIAGES = ('Married', 'Not Married')
+
+    def __init__(self, name, age, married):
+        self.name = name
+        self.age = age
+        self.married = married
+    
+    def __repr__(self):
+        return f'<Class Person({self.name}, {self.age} years old. {self.married})>'
+    
+    @classmethod
+    def married(cls, name, age):
+        return cls(name, age, married=cls.MARRIAGES[0])
+
+    @staticmethod
+    def occupation(occupation):
+        return f'<Class Person({occupation})>'
+
+person = Person('Joko', 29, 'Married')
+print(person.__repr__())
+print(Person.married('Andi', 35))
+print(Person.occupation('Farmer'))
