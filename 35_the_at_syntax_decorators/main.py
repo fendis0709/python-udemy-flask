54d5747d34677bf410921b459f6b76a6a29a5ee7
Fendi Septiawan <fendi.septiawan0709@gmail.com>
2020-10-13 17:54:02 +0700

    The 'at' syntact for decorators

--- 35_the_at_syntax_decorators/main.py
+++ 35_the_at_syntax_decorators/main.py
@@ -1,17 +1,24 @@
+import functools
+
 user = {'name': 'Joko', 'level': 'user'}
-# user = {'name': 'Joko', 'level': 'admin'}
+user = {'name': 'Joko', 'level': 'admin'}
 
 
 def secureAdminPassword(func):
+    # Remember always using this function tools when using function decorator
+    @functools.wraps(func)
     def secure():
         if(user['level'] == 'admin'):
             return func()
     return secure
 
 
+# @<name_of_method_decorator>
+@secureAdminPassword
 def getAdminPassword():
     return '12345'
 
 
 getAdminPassword = secureAdminPassword(getAdminPassword)
+print(getAdminPassword.__name__)
 print(getAdminPassword())
