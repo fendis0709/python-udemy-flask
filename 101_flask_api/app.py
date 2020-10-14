d6403bff18d7fddd8e5279c70a026e4e17810f91
Fendi Setiawan <fendi.septiawan0709@gmail.com>
2020-10-14 14:05:12 +0700

    Routing using flask route

--- 101_flask_api/app.py
+++ 101_flask_api/app.py
@@ -1,11 +1,68 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
+
+students = [
+    {
+        'name': 'Joko',
+        'gender': 'Male',
+        'class': 'XII - A 1',
+        'grades': [
+            {
+                'name': 'mathematic',
+                'score': 90
+            },
+            {
+                'name': 'science',
+                'score': 95
+            }
+        ]
+    }
+]
 
 
 @app.route('/')
 def main():
     return 'Hello, world!'
+
+
+# Get students list
+# GET   : {host}/students
+@app.route('/students')
+def getStudent():
+    # return 'student-list'
+    return jsonify({
+        'students': students
+    })
+
+
+# Get student detail
+# GET : {host}/students/<int:id>
+@app.route('/students/<int:id>')
+def getStudentDetail(id):
+    return f'student-detail:{id}'
+
+
+# Register new student
+# POST : {host}/students
+@app.route('/students', methods=['POST'])
+def registerStudent():
+    return 'student-register'
+
+
+# Update student
+# PUT : {host}/students/<int:id>
+@app.route('/students/<int:id>', methods=['PUT'])
+def updateStudent(id):
+    return f'student-update:{id}'
+
+
+# Delete student
+# DELETE : {host}/students/<int:id>
+@app.route('/students/<int:id>', methods=['DELETE'])
+def deleteStudent(id):
+    return f'student-delete:{id}'
 
 
 app.run(port=5000)
