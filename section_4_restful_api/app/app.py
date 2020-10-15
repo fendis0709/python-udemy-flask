d9ce84484456fb4fd96734adc357cc8aa975df0e
fendis0709 <fendi.septiawan0709@gmail.com>
2020-10-15 16:01:51 +0700

    Create Restful API using resource rules

--- section_4_restful_api/app/app.py
+++ section_4_restful_api/app/app.py
@@ -1,19 +1,53 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
+students = [
+    {
+        'id': 1,
+        'name': 'Joko',
+    }
+]
+
 
 class Student(Resource):
-    def get(self, slug):
+    # Find student by ID
+    def get(self, id):
+        for student in students:
+            if student['id'] == id:
+                return {
+                    'student': student
+                }, 200
         return {
-            'name': 'Joko',
-            'slug': slug
+            'student': None
+        }, 404
+
+
+class Students(Resource):
+    # Get all students
+    def get(self):
+        return {
+            'students': students
+        }, 200
+
+    # Register new student
+    def post(self):
+        data = request.get_json()
+        counter = len(students)
+        student = {
+            'id': counter + 1,
+            'name': data['name'],
         }
+        students.append(student)
+        return {
+            'students': students
+        }, 201
 
 
-api.add_resource(Student, '/students/<string:slug>')
+api.add_resource(Students, '/students')
+api.add_resource(Student, '/students/<int:id>')
 
 
 app.run()
