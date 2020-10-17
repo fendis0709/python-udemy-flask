7e4a30c4d943cc698571bdf2953b351d84a7eefd
fendis0709 <fendi.septiawan0709@gmail.com>
2020-10-17 15:20:20 +0700

    Insert users to database

--- section_5_connecting_api_to_database/database.py
+++ section_5_connecting_api_to_database/database.py
@@ -1,6 +1,18 @@
 import sqlite3
 
-connection = sqlite3.connect('data.db')
+connection = sqlite3.connect('./data.db')
 
-createTableUsers = 'CREATE TABLE users (id int, username text, password, text)'
+createTableUsers = 'CREATE TABLE users (id int, username text, password text)'
 connection.cursor().execute(createTableUsers)
+
+userData = (1, "joko", "12345")
+insertUser = 'INSERT INTO users VALUES (?, ?, ?)'
+connection.cursor().execute(insertUser, userData)
+
+getUsers = 'SELECT * FROM users'
+for user in connection.cursor().execute(getUsers):
+    print(user)
+
+connection.commit()
+
+connection.close()
